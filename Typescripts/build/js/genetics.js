887fba9f16c7e4475490b59cb0aa7b52e59363b5
coding-jhuunn <139361270+coding-jhuunn@users.noreply.github.com>
2023-10-23 21:55:36 +0800

    remove some files

--- Typescripts/build/js/genetics.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const checkObj = (item) => {
-    if (typeof item === 'object' && Array.isArray(item) && item !== null) {
-        return true;
-    }
-    return false;
-};
-console.log(checkObj([1, 23]));
--- Typescripts/src/genetics.ts
+++
@@ -1,8 +0,0 @@
-const checkObj = <T> (item:T):boolean => {
-    if(typeof item === 'object'&& Array.isArray(item)&& item !==null){
-        return true;
-    }
-    return false;
-}
-
-console.log(checkObj([1,23]))
